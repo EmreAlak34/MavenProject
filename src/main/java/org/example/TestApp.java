ccaca110e94feafaaac6c73c04c9560ba3c8be0e
Emre Alak <alakemre@gmail.com>
2024-11-24 13:53:59 +0100

    change greeting

--- src/main/java/org/example/TestApp.java
+++ src/main/java/org/example/TestApp.java
@@ -4,6 +4,6 @@ package org.example;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class TestApp {
     public static void main(String[] args) {
-        System.out.println("Hello Emrea");
+        System.out.println("Hello Emrealak");
         }
     }
