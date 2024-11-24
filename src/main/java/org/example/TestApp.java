4647dae220ae7435d73c7199707c8009331c88dc
Emre Alak <alakemre@gmail.com>
2024-11-24 13:57:32 +0100

    add comment

--- src/main/java/org/example/TestApp.java
+++ src/main/java/org/example/TestApp.java
@@ -5,5 +5,6 @@ package org.example;
 public class TestApp {
     public static void main(String[] args) {
         System.out.println("Hello Emrealak");
+        System.out.println("Automatisierung funktioniert");
         }
     }
